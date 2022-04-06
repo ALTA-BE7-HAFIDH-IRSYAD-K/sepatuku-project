18a462428e35beec74f8df424d402ef4ecb9cab9
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-06 16:09:08 +0700

    membuat hash password

--- service/user/interface.go
+++ service/user/interface.go
@@ -6,5 +6,5 @@ type UserServiceInterface interface {
 	GetUser(id int) (_entities.User, int, error)
 	DeleteUser(id int) (_entities.User, error)
 	CreateUser(user _entities.User) (_entities.User, error)
-	UpdatedUser(users _entities.User, id int) (_entities.User, error)
+	UpdatedUser(user _entities.User, id int) (_entities.User, error)
 }
--- service/user/user.go
+++ service/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"sepatuku-project/delivery/response"
 	_entities "sepatuku-project/entity/user"
 	_userRepository "sepatuku-project/repository/user"
 )
@@ -16,20 +17,24 @@ func NewUserService(userRepo _userRepository.UserRepositoryInterface) UserServic
 }
 
 func (uuc *UserService) GetUser(id int) (_entities.User, int, error) {
-	users, rows, err := uuc.userRepository.GetUser(id)
-	return users, rows, err
+	user, rows, err := uuc.userRepository.GetUser(id)
+	return user, rows, err
 }
 
 func (uuc *UserService) DeleteUser(id int) (_entities.User, error) {
-	users, err := uuc.userRepository.DeleteUser(id)
-	return users, err
+	user, err := uuc.userRepository.DeleteUser(id)
+	return user, err
 }
 func (uuc *UserService) CreateUser(user _entities.User) (_entities.User, error) {
-	users, err := uuc.userRepository.CreateUser(user)
-	return users, err
+	user, err := uuc.userRepository.CreateUser(user)
+	password, err := response.HashPassword(user.Password)
+	user.Password = password
+	return user, err
 }
-func (uuc *UserService) UpdatedUser(users _entities.User, id int) (_entities.User, error) {
-	users.ID = uint(id)
-	users, err := uuc.userRepository.UpdatedUser(users)
-	return users, err
+func (uuc *UserService) UpdatedUser(user _entities.User, id int) (_entities.User, error) {
+	user.ID = uint(id)
+	user, err := uuc.userRepository.UpdatedUser(user)
+	password, err := response.HashPassword(user.Password)
+	user.Password = password
+	return user, err
 }

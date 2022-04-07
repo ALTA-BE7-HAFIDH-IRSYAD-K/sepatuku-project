787c04d137537fd6fa74f906f7b99e8f678a279a
nasrul0807 <nasrulmuhammad748@gmail.com>
2022-04-07 14:55:36 +0700

    mengubah configs

--- configs/configs.go
+++ configs/configs.go
@@ -1,20 +1,18 @@
 package configs
 
 import (
+	"os"
 	"sync"
-
-	"github.com/labstack/gommon/log"
-	"github.com/spf13/viper"
 )
 
 type AppConfig struct {
-	Port      int    `yaml:"port"`
+	Port      string `yaml:"port"`
 	SecretJWT string `yaml:"secretjwt"`
 	Database  struct {
 		Driver   string `yaml:"driver"`
 		Name     string `yaml:"name"`
 		Address  string `yaml:"address"`
-		Port     int    `yaml:"port"`
+		Port     string `yaml:"port"`
 		Username string `yaml:"username"`
 		Password string `yaml:"password"`
 	}
@@ -36,28 +34,28 @@ func GetConfig() *AppConfig {
 
 func initConfig() *AppConfig {
 	var defaultConfig AppConfig
-	defaultConfig.Port = 8080
-	defaultConfig.SecretJWT = "S3CR3T"
-	defaultConfig.Database.Driver = "mysql"
-	defaultConfig.Database.Name = "sepatuku"
-	defaultConfig.Database.Address = "localhost"
-	defaultConfig.Database.Port = 3307
-	defaultConfig.Database.Username = "root"
-	defaultConfig.Database.Password = "Khairuddin"
+	defaultConfig.Port = os.Getenv("APP_PORT")
+	defaultConfig.SecretJWT = os.Getenv("SECRET_JWT")
+	defaultConfig.Database.Driver = os.Getenv("DB_DRIVER")
+	defaultConfig.Database.Name = os.Getenv("DB_NAME")
+	defaultConfig.Database.Address = os.Getenv("DB_ADDRESS")
+	defaultConfig.Database.Port = os.Getenv("DB_PORT")
+	defaultConfig.Database.Username = os.Getenv("DB_USERNAME")
+	defaultConfig.Database.Password = os.Getenv("DB_PASSWORD")
 
-	viper.SetConfigType("yaml")
-	viper.SetConfigName("config")
-	viper.AddConfigPath("./configs/")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Info("failed to open file")
-		return &defaultConfig
-	}
+	// viper.SetConfigType("yaml")
+	// viper.SetConfigName("config")
+	// viper.AddConfigPath("./configs/")
+	// if err := viper.ReadInConfig(); err != nil {
+	// 	log.Info("failed to open file")
+	// 	return &defaultConfig
+	// }
 
-	var finalConfig AppConfig
-	err := viper.Unmarshal(&finalConfig)
-	if err != nil {
-		log.Info("failed to extract external config, use default value")
-		return &defaultConfig
-	}
-	return &finalConfig
+	// var finalConfig AppConfig
+	// err := viper.Unmarshal(&finalConfig)
+	// if err != nil {
+	// 	log.Info("failed to extract external config, use default value")
+	// 	return &defaultConfig
+	// }
+	return &defaultConfig
 }

8ebfa4b5301907b59623fe51ca3a9b91eebb7f65
hafidhirsyad <hafidhkh97@gmail.com>
2022-04-07 20:12:16 +0700

    fix order and cart handler in main.go

--- entity/cart/cart.go
+++ entity/cart/cart.go
@@ -7,9 +7,9 @@ import (
 
 type Cart struct {
 	*gorm.Model
-	UserId     uint              `json:"user_id" form:"user_id"`
-	ProductId  uint              `json:"product_id" form:"product_id"`
-	Quantity   uint              `json:"quantity" form:"quantity"`
-	TotalPrice uint              `json:"total_price" form:"total_price"`
-	Product    []product.Product `json:"product" form:"product" gorm:"foreignKey:ID;references:ProductId"`
+	UserId     uint            `json:"user_id" form:"user_id"`
+	ProductId  uint            `json:"product_id" form:"product_id"`
+	Quantity   uint            `json:"quantity" form:"quantity"`
+	TotalPrice uint            `json:"total_price" form:"total_price"`
+	Product    product.Product `json:"product" form:"product" gorm:"foreignKey:ProductId;references:ID"`
 }
